9b6dcdd51218488759a7c3ceaee7f230dedf796a
Ali Biadar <alibiadar86@gmail.com>
2025-01-19 15:42:48 +0330

    Additional changes

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
       },
       devOptions: {
-        enabled: false,
+        enabled: true,
       },
       manifest: {
         name: 'My PWA App',
